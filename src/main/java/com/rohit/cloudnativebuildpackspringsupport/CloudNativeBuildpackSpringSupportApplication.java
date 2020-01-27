86e19db993469498deb1a110da54bf0af4d924ea
Rohit <hitro.jain@gmail.com>
2020-01-27 15:44:58 +0530

    first commit

---
+++ src/main/java/com/rohit/cloudnativebuildpackspringsupport/CloudNativeBuildpackSpringSupportApplication.java
@@ -0,0 +1,13 @@
+package com.rohit.cloudnativebuildpackspringsupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNativeBuildpackSpringSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNativeBuildpackSpringSupportApplication.class, args);
+	}
+
+}
